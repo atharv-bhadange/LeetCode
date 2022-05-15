f93d4148c614eec6c1d7b980b99249f6c9a4a201
atharv-bhadange <bhadange.atharv@gmail.com>
2022-05-15 11:20:50 +0530

    Daily challenge May 12

---
+++ Medium/47_permuteUnique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size()-1;
+        set<vector<int>> ans;
+        auto temp = nums;
+        do
+        {
+            next_permutation(temp.begin(),temp.end());
+            ans.insert(temp);
+        }
+        while(temp!=nums);
+        vector<vector<int>> v;
+        for(auto x: ans) v.push_back(x);
+        return v;
+    }
+};
