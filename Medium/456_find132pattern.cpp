795f8134ff6d91879ab1589c3efe6e5d9071e7ca
atharv-bhadange <bhadange.atharv@gmail.com>
2022-05-15 11:00:45 +0530

    Daily challenge May 7

---
+++ Medium/456_find132pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int> st;
+        int mid = INT_MIN;
+        for(int i=nums.size()-1; i>=0; i--)
+        {
+            if(mid>nums[i])
+                return true;
+           
+            while(!st.empty() && nums[i]>st.top())
+            {
+                mid = st.top();
+                st.pop();
+            }
+            st.push(nums[i]);     
+        }
+        
+        return false;
+    }
+};
