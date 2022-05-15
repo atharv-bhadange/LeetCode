c051954a3c2175d341a0fc5e873857b40eaedc7a
atharv-bhadange <bhadange.atharv@gmail.com>
2022-05-15 11:19:20 +0530

    Daily challenge May 11

---
+++ Medium/1641_countVowelStrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int NcR(int n, int r)
+{
+    int p = 1, k = 1;
+
+    if (n - r < r)
+        r = n - r;
+
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+
+            int m = __gcd(p, k);
+
+            p /= m;
+            k /= m;
+
+            n--;
+            r--;
+        }
+    }
+
+    else
+        p = 1;
+    return p;
+}
+    
+    int countVowelStrings(int r) {
+        int n=5;
+        return NcR(n+r-1,r);
+    }
+};
