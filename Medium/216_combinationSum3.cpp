e1bdc487a60d1baf3c3a854a3cca8ad18b1433c4
atharv-bhadange <bhadange.atharv@gmail.com>
2022-05-15 11:16:33 +0530

    Daily challenge May 10

---
+++ Medium/216_combinationSum3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    vector<vector<int>> ans;
+public:
+    void recur(int n,int k, vector<int> &sub, int num)
+    {
+        if(k==0)
+        {
+            if(accumulate(sub.begin(),sub.end(),0)==n)
+            {
+                ans.push_back(sub);
+                // sub.clear();
+            }
+            return;
+        }
+        if(num>9){
+            return;
+        }
+        sub.push_back(num);
+        recur(n,k-1,sub,num+1);
+        sub.pop_back();
+        recur(n,k,sub,num+1);
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> sub;
+        recur(n,k,sub,1);
+        return ans;
+    }
+};
