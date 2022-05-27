123d6e9dd17b7792284b6621cac7d778f2068e34
atharv-bhadange <bhadange.atharv@gmail.com>
2022-05-27 09:02:53 +0530

    Daily challenge May 26

---
+++ Easy/191_hammingWeight.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        string s = "";
+        while (n)
+        {
+            int l = n % 2;
+            s += to_string(l);
+            n /= 2;
+        }
+        int ans = 0;
+        // cout<<s<<endl;
+        for (int i = 0; i < s.size(); i++)
+            if (s[i] == '1')
+                ans++;
+        return ans;
+    }
+};
