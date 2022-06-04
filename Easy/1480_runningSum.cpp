1b6d7a63cc4dd04105ed3458db037e8be5149d6d
atharv-bhadange <bhadange.atharv@gmail.com>
2022-06-04 11:01:03 +0530

    Daily challenge June 1

---
+++ Easy/1480_runningSum.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        vector<int> r(n, 0);
+
+        r[0] = nums[0];
+
+        for (int i = 1; i < n; i++)
+
+        {
+
+            r[i] = r[i - 1] + nums[i];
+        }
+
+        // r.pop_front();
+
+        return r;
+    }
+};
