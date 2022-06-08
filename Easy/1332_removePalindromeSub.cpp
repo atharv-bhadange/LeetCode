2d20fe88a2d7469c8d8789708f15b595ef3b6b76
atharv-bhadange <bhadange.atharv@gmail.com>
2022-06-08 11:18:28 +0530

    Daily challenge June 8

---
+++ Easy/1332_removePalindromeSub.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int removePalindromeSub(string s)
+    {
+        for (int i = 0; i < s.size() / 2 + 1; i++)
+        {
+            if (s[i] != s[s.size() - i - 1])
+                return 2;
+        }
+        return 1;
+    }
+};
