a74b29b54925dcfc84c48eb165e6866e4a688d66
atharv-bhadange <bhadange.atharv@gmail.com>
2022-06-11 12:51:43 +0530

    Daily challenge June 11

---
+++ Medium/1658_minOperations.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int x)
+    {
+        int n = nums.size();
+        int sum=0;
+        for(auto &i:nums)
+            sum+=i;
+        int k = sum - x;
+        int maxSum = -1, curSum = 0;
+        for (int l = 0, r = 0; r < n; r++)
+        {
+            curSum += nums[r];
+            while (l <= r && curSum > k)
+                curSum -= nums[l++];
+            if (curSum == k)
+                maxSum = max(maxSum, r - l + 1);
+        }
+
+        return maxSum == -1 ? -1 : n - maxSum;
+    }
+};
+
+// question all about change of perspective 
+// find longest continuous subset with sum == totsum - x
+// other than that is our answer
