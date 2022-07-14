e59fbf6df990c75c19f2fc3d6fc0c5ddde6cc9cb
atharv-bhadange <bhadange.atharv@gmail.com>
2022-07-14 14:24:46 +0530

    Daily challenge July 6

--- Easy/509_fib.cpp
+++ Easy/509_fib.cpp
@@ -9,4 +9,16 @@ public:
             return 1;
         return fib(n - 1) + fib(n - 2);
     }
+
+    int fibI(int n)
+    {
+        int a[n + 1];
+        a[0] = 0;
+        a[1] = 1;
+        for (int i = 2; i <= n; i++)
+            a[i] = a[i - 1] + a[i - 2];
+
+        int output = a[n];
+        return output;
+    }
 };
