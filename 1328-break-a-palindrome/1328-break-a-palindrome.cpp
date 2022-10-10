40f46e8d543b519634ce2e8e546ccf356ec7bf8e
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2022-10-10 19:27:26 +0530

    Time: 6 ms (16.14%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        int n = s.size();
+        if(n==1)
+            return "";
+        
+        int i=0;
+        for(int i=0; i<n/2; i++) {
+            if(s[i]!='a'){
+                s[i] = 'a';
+                return s;
+            }
+        }
+        
+        s[n-1] = 'b';
+        return s;
+        
+            
+    }
+};
