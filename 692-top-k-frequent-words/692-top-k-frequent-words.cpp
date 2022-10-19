459b8aea8e5033bd800e3c8ab443f116a57b241e
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2022-10-19 09:50:38 +0530

    Time: 29 ms (37.61%), Space: 12.6 MB (56.15%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    static bool cmp(pair<string,int>& a, pair<string,int>& b) {
-        if(a.second==b.second)
-            return a.first<b.first;
-        return a.second>b.second;
-    }
+    // static bool cmp(pair<int,string>& a, pair<int,string>& b) {
+    //     if(a.first==b.first)
+    //         return a.second<b.second;
+    //     return a.first>b.first;
+    // }
+    
+    struct Comp {
+        Comp() {}
+        ~Comp() {}
+        bool operator() (const pair<string,int>& a, const pair<string,int>& b) {
+            if(a.second==b.second)
+                return a.first<b.first;
+            return a.second>b.second;
+        }  
+    };
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string,int> mp;
         for(auto& x: words) 
             mp[x]++;
         
-        vector<pair<string,int>> vp;
-        for(auto& x : mp) 
-            vp.push_back({x.first,x.second});
-        
-        sort(vp.begin(), vp.end(), cmp);
+        priority_queue<pair<string,int>, vector<pair<string,int>>, Comp> pq;
+        for(auto& x: mp) {
+            pq.emplace(x);
+            if(pq.size() > k)
+                pq.pop();
+        }
         
         vector<string> ans;
-        for(int i=0; i<k; i++) {
-            ans.push_back(vp[i].first);
+        while(!pq.empty()) {
+            ans.push_back(pq.top().first);
+            pq.pop();
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
