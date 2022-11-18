902847c279a9c08487ed938c6c1cdb12fa1b66a6
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2022-11-18 23:51:01 +0530

    Time: 8 ms (11.77%), Space: 6.1 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    vector<int> primeFactors(int n) 
-    { 
-        vector<int> res;
-        while (n%2 == 0) 
-        { 
-            res.push_back(2); 
-            n = n/2; 
-        } 
-
-        for (int i = 3; i <= sqrt(n); i = i+2) 
-        { 
-            while (n%i == 0) 
-            { 
-                res.push_back(i); 
-                n = n/i; 
-            } 
-        } 
-
-        if (n > 2) 
-            res.push_back(n); 
-        
-        return res;
-    } 
+    int kdwd(int divid, int divis) {
+        while(divid%divis==0) 
+            divid/=divis;
+        return divid;
+    }
     
     bool isUgly(int n) {
         if(n<=0)
             return false;
-        vector<int> res = primeFactors(n);
         
-        for(int i=0; i<res.size(); i++)
-            if(res[i]!=2 && res[i]!=3 && res[i]!=5)
-                return false;
+        vector<int> ugly = {2, 3, 5};
+        for(auto &x : ugly) 
+            n = kdwd(n, x);
         
-        return true;
+        return n==1;
     }
 };
