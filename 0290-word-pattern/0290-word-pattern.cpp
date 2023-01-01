b7d7b35718b48c6d741952e2b4377ceb0da726a7
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2023-01-01 10:29:42 +0530

    Time: 6 ms (11.86%), Space: 6.5 MB (53.35%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> vs;
+        string temp = "";
+        for(auto& x : s) {
+            if(x==' ') {
+                vs.push_back(temp);
+                temp = "";
+            }
+            else {
+                temp += x;
+            }
+        }
+        vs.push_back(temp);
+        if(vs.size() != pattern.size())
+            return false;
+        // unordered_map<char, string> m;
+        unordered_map<string, char> m;
+        vector<bool> done(26,0);
+        for(int i=0; i<pattern.size(); i++) {
+            if(m.find(vs[i]) == m.end() ) {
+                m[vs[i]] = pattern[i];
+                if(done[pattern[i]-'a'])
+                    return false;
+                done[pattern[i]-'a'] = 1;
+            }
+            else {
+                cout<<i<<vs[i]<<m[vs[i]]<<endl;
+                
+                if(m[vs[i]] != pattern[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
