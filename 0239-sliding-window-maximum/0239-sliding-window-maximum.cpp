05ed5039d3ae92b576b99465f256ad74bea928c6
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2023-07-21 22:27:10 +0530

    Time: 464 ms (22.15%), Space: 194.1 MB (11.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& a, int k) {
-        if(k==1)
-            return a;
+
         int i = 0, j = 0;
         list<int> q;
         int n = a.size();
         vector<int> ans;
         while(j<n) {
-            // for(auto& x: q) 
-            //     cout<<x<<"+ ";
             while(!q.empty() && q.back() < a[j]) {
                     q.pop_back();
-                // cout<<a[j]<<"* ";
             }
-            // cout<<endl;
             q.push_back(a[j]);
             if(j-i+1 != k) {
                 j++;
@@ -28,8 +23,6 @@ public:
                 i++;
                 j++;
             }
-            // for(auto& x: q) 
-            //     cout<<x<<"* ";
         }
         
         return ans;
