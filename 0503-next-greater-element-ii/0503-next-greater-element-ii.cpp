9339eb0e0bbe4ebef304a911f6805864c70a1f59
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2023-07-31 18:28:53 +0530

    Time: 23 ms (99.11%), Space: 23.9 MB (71.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,11 +5,11 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         for(int i=2*n-1; i>=0; i--) {
-            while(!st.empty() && nums[st.top()] <= nums[i%n]) {
+            while(!st.empty() && st.top() <= nums[i%n]) {
                 st.pop();
             }
-            ans[i%n] = st.empty() ? -1 : nums[st.top()];
-            st.push(i%n);
+            ans[i%n] = st.empty() ? -1 : st.top();
+            st.push(nums[i%n]);
         }
         
         return ans;
