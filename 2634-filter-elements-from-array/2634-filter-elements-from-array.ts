cf0516e2cac76df66615773de6462ba664233f1e
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2023-08-16 13:58:36 +0530

    Time: 62 ms (48.06%), Space: 43 MB (55.75%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
@@ -0,0 +1,10 @@
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    let ans = [];
+    for(let i: number=0; i<arr.length; i++) {
+        if(fn(arr[i],i)) {
+            ans.push(arr[i]);
+        }
+    }
+    
+    return ans;
+};
